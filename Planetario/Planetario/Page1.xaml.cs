d5a05b525c7b172842785b90c87d10b2a55fe795
nattechnology <50607910+nattechnology@users.noreply.github.com>
2019-05-23 22:50:19 -0500

    aplicacion
    Codigo de app

---
+++ Planetario/Planetario/Page1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Planetario
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : TabbedPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Planetario/Planetario/Planetario/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Planetario
+{
+   
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Sala_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1(),true);
+        }
+
+        private async void Eventos_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Event(),true);
+        }
+
+        private async void Conocenos_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cono(),true);
+        }
+    }
+}
